e16df2f51db7c5d1904a33e036a46e9cb0a990c2
Oliver Joseph Ash <oliverjash@gmail.com>
2015-06-14 16:33:23 +0100

    Match explicit tab

--- popup.js
+++ popup.js
@@ -1,6 +1,6 @@
 export default function () {
     const getData = () => new Promise((resolve, reject) => {
-        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.query({ currentWindow: true, url: 'http://localhost:9000/analytics/abtests' }, function (tabs) {
             chrome.tabs.sendMessage(tabs[0].id, { action: 'getData' }, resolve);
         });
     });
