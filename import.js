a8ada74ea4be5a3bff817f33520212aa4121410d
Oliver Joseph Ash <oliverjash@gmail.com>
2015-06-18 01:29:17 +0100

    Use SystemJS CSP version

--- import.js
+++ import.js
@@ -1,11 +1,3 @@
-System.config({
-    // TODO: Use CSP version
-    // https://github.com/systemjs/systemjs/issues/517#issuecomment-111835690
-    meta: {
-        'build': { format: 'register' }
-    }
-});
-
 System.import('popup').then(function (module) {
     module.default();
 });
