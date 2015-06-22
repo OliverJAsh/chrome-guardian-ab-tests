5fc34740cde0f69d4a03c7132cb86f0e8c5d6b15
Oliver Joseph Ash <oliverjash@gmail.com>
2015-06-22 16:07:10 +0100

    Tidy up

--- popup.js
+++ popup.js
@@ -26,7 +26,8 @@ const sendMessageToPage = message => new Promise(resolve =>
     getTab().then(tab => chrome.tabs.sendMessage(tab.id, message, resolve)));
 
 const getData = () => sendMessageToPage({ action: 'getData' });
-const setParticipations = participations => sendMessageToPage({ action: 'setParticipations', data: { participations } });
+const setParticipations = participations =>
+    sendMessageToPage({ action: 'setParticipations', data: { participations: participations.toJS() } });
 
 // TODO: Why is this needed?
 const ih = (tagName, options, children) => {
@@ -45,6 +46,7 @@ export default function () {
                 test
                     .updateIn(['variants'], variants => variants.map(variant => variant.get('id')))
                     .updateIn(['variants'], variants => variants.push('notintest')));
+        const participations = Im.fromJS(data.participations);
 
         function tableComponent() {
             const select$ = new Rx.Subject();
@@ -85,16 +87,20 @@ export default function () {
         function view() {
             const table = tableComponent();
 
-            const participations$ = table.intents.select$.startWith({}).scan(Im.fromJS(data.participations), (participations, selectedTest) =>
-                participations.setIn([selectedTest.id], selectedTest.variant));
-            participations$.subscribe(participations => console.log(participations.toJS()));
-            participations$.subscribe(participations => setParticipations(participations.toJS()));
+            const participations$ = table.intents.select$
+                .startWith({})
+                .scan(participations,
+                    (participations, selectedTest) => participations.set(selectedTest.id, selectedTest.variant));
+
+            participations$.subscribe(setParticipations);
+
+            const updateTestVariant = (test, participations) => {
+                const variant = participations.get(test.get('id'));
+                return test.set('variant', variant);
+            };
 
             const tests$ = participations$.scan(tests, (tests, participations) =>
-                tests.map(test =>
-                    test.set('variant', participations.find((variant, participation) =>
-                        participation === test.get('id')))));
-            tests$.subscribe(tests => console.table(tests.toJS()));
+                tests.map(test => updateTestVariant(test, participations)));
 
             const tree$ = table.view$(tests$);
 
