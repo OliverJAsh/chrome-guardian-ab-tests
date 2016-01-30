6330035e797fd3f7816f929d2524cf62ae7b0daf
Oliver Joseph Ash <oliverjash@gmail.com>
2016-01-30 18:21:38 +0000

    Migrate to Cycle.js

--- config.js
+++ config.js
@@ -13,6 +13,9 @@ System.config({
   },
 
   map: {
+    "@cycle/core": "npm:@cycle/core@6.0.0",
+    "@cycle/dom": "npm:@cycle/dom@9.0.2",
+    "@cycle/isolate": "npm:@cycle/isolate@1.2.0",
     "babel": "npm:babel-core@5.8.35",
     "babel-runtime": "npm:babel-runtime@5.8.35",
     "core-js": "npm:core-js@1.2.6",
@@ -21,7 +24,7 @@ System.config({
     "rx": "npm:rx@4.0.7",
     "rx-dom": "npm:rx-dom@7.0.3",
     "vdom-virtualize": "npm:vdom-virtualize@0.0.10",
-    "virtual-dom": "npm:virtual-dom@2.0.1",
+    "virtual-dom": "npm:virtual-dom@2.1.1",
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.3.0"
     },
@@ -33,6 +36,17 @@ System.config({
     },
     "github:jspm/nodelibs-util@0.1.0": {
       "util": "npm:util@0.10.3"
+    },
+    "npm:@cycle/core@6.0.0": {
+      "rx": "npm:rx@4.0.7"
+    },
+    "npm:@cycle/dom@9.0.2": {
+      "hyperscript-helpers": "npm:hyperscript-helpers@2.0.3",
+      "matches-selector": "npm:matches-selector@1.0.0",
+      "vdom-parser": "npm:vdom-parser@1.2.1",
+      "vdom-to-html": "npm:vdom-to-html@2.2.0",
+      "virtual-dom": "npm:virtual-dom@2.1.1",
+      "x-is-array": "npm:x-is-array@0.1.0"
     },
     "npm:assert@1.3.0": {
       "util": "npm:util@0.10.3"
@@ -50,7 +64,7 @@ System.config({
       "assert": "github:jspm/nodelibs-assert@0.1.0",
       "camelize": "npm:camelize@1.0.0",
       "string-template": "npm:string-template@0.2.1",
-      "xtend": "npm:xtend@4.0.0"
+      "xtend": "npm:xtend@4.0.1"
     },
     "npm:ev-store@7.0.0": {
       "individual": "npm:individual@3.0.0"
@@ -63,6 +77,9 @@ System.config({
     },
     "npm:next-tick@0.2.2": {
       "process": "github:jspm/nodelibs-process@0.1.2"
+    },
+    "npm:param-case@1.1.2": {
+      "sentence-case": "npm:sentence-case@1.1.3"
     },
     "npm:path-browserify@0.0.0": {
       "process": "github:jspm/nodelibs-process@0.1.2"
@@ -77,14 +94,26 @@ System.config({
     "npm:rx@4.0.7": {
       "process": "github:jspm/nodelibs-process@0.1.2"
     },
+    "npm:sentence-case@1.1.3": {
+      "lower-case": "npm:lower-case@1.1.3"
+    },
     "npm:util@0.10.3": {
       "inherits": "npm:inherits@2.0.1",
       "process": "github:jspm/nodelibs-process@0.1.2"
     },
-    "npm:vdom-virtualize@0.0.10": {
-      "virtual-dom": "npm:virtual-dom@2.0.1"
+    "npm:vdom-parser@1.2.1": {
+      "virtual-dom": "npm:virtual-dom@2.1.1"
     },
-    "npm:virtual-dom@2.0.1": {
+    "npm:vdom-to-html@2.2.0": {
+      "escape-html": "npm:escape-html@1.0.3",
+      "param-case": "npm:param-case@1.1.2",
+      "virtual-dom": "npm:virtual-dom@2.1.1",
+      "xtend": "npm:xtend@4.0.1"
+    },
+    "npm:vdom-virtualize@0.0.10": {
+      "virtual-dom": "npm:virtual-dom@2.1.1"
+    },
+    "npm:virtual-dom@2.1.1": {
       "browser-split": "npm:browser-split@0.0.1",
       "error": "npm:error@4.4.0",
       "ev-store": "npm:ev-store@7.0.0",
--- popup.js
+++ popup.js
@@ -1,25 +1,18 @@
+// Mini Cycle.js
+// http://jsbin.com/joyowa/6/edit?js,output
+// BMI
+// http://jsbin.com/bicoziniqu/1/edit?js,output
+
 /*eslint-disable no-console*/
 /*eslint-env browser*/
 /*eslint-disable new-cap*/
 /*global chrome*/
 import Rx from 'rx-dom';
-import h from 'virtual-dom/h';
-import diff from 'virtual-dom/diff';
-import patch from 'virtual-dom/patch';
-import virtualize from 'vdom-virtualize';
 import Im from 'immutable';
 
-const getTab = () => new Promise((resolve, reject) => {
-    chrome.tabs.query({ currentWindow: true, active: true }, tabs => resolve(tabs[0]));
-});
-
-const sendMessageToPage =
-    message => new Promise(resolve =>
-        getTab().then(tab => chrome.tabs.sendMessage(tab.id, message, resolve)));
-
-const getData = () => sendMessageToPage({ action: 'getData' });
-const setParticipations = participations =>
-    sendMessageToPage({ action: 'setParticipations', data: { participations: participations.toJS() } });
+import Cycle from '@cycle/core';
+import isolate from '@cycle/isolate';
+import {makeDOMDriver, h} from '@cycle/dom';
 
 // TODO: Why is this needed?
 const ih = (tagName, options, children) => {
@@ -30,106 +23,207 @@ const ih = (tagName, options, children) => {
     return h(tagName, options, children.toJS ? children.toJS() : children);
 };
 
-const render = data => {
-    // Normalize data
-    const initialTests = Im.fromJS(data.tests)
-        .map(test =>
-            test
-                .updateIn(['variants'], variants => variants.map(variant => variant.get('id')))
-                .updateIn(['variants'], variants => variants.push('notintest')));
-    const initialParticipations = Im.fromJS(data.participations);
+// Get data from and send data to the tab of this Chrome extension
+const tabDriver = function (data$) {
+    //
+    // Write side effects
+    //
+    const getTab = () => new Promise((resolve) => {
+        chrome.tabs.query({ currentWindow: true, active: true }, tabs => resolve(tabs[0]));
+    });
 
-    function tableComponent() {
-        const select$ = new Rx.Subject();
-        const headers = ['id', 'variants'];
+    const sendMessageToPage =
+        message => new Promise(resolve =>
+            getTab().then(tab => chrome.tabs.sendMessage(tab.id, message, resolve)));
 
-        function variantButtonElement(test, variant, selectedVariant) {
-            return h('label', {
-                className: 'mdl-radio mdl-js-radio mdl-js-ripple-effect'
-            }, [
-                h('input', {
-                    type: 'radio',
-                    name: `${test.get('id')}[variant]`,
-                    onchange: () => select$.onNext({ id: test.get('id'), variant }),
-                    checked: variant === selectedVariant,
-                    className: 'mdl-radio__button'
-                }, variant),
-                variant
-            ]);
+    // TEST:
+    // const setParticipations = () => {};
+    const setParticipations = participations =>
+        sendMessageToPage({ action: 'setParticipations', data: { participations: participations.toJS() } });
+
+    data$.subscribe(data => {
+        const { participations } = data;
+        if (participations) {
+            console.log('Setting participations', JSON.stringify(participations));
+            setParticipations(participations);
         }
+    });
 
-        function rowElement(test, participations) {
-            const cellElement =
-                header => ih('td', { className: 'mdl-data-table__cell--non-numeric' },
+    //
+    // Read side effects
+    //
+
+    // TEST:
+    /* eslint-disable */
+    // const mockData = {"participations":{"FrontsOnArticles2":"notintest","RemoveStickyNav":"new","RelatedVariants":"notintest","PrebidPerformance":"notintest","LiveblogToast":"notintest","UserzoomSurveyMessageV3":"notintest"},"tests":[{"id":"FrontsOnArticles2","start":"2015-12-08","expiry":"2016-4-1","author":"Josh Holder","description":"Inject fronts containers on articles","audience":0.06,"audienceOffset":0.3,"successMeasure":"","audienceCriteria":"All users","dataLinkNames":"","idealOutcome":"","variants":[{"id":"control"},{"id":"oneAndThree"},{"id":"twoAndTwo"}]},{"id":"RemoveStickyNav","start":"2015-1-07","expiry":"2016-3-1","author":"Josh Holder","description":"0% AB test for removing the sticky nav","audience":0.5,"audienceOffset":0.5,"successMeasure":"","audienceCriteria":"All users","dataLinkNames":"","idealOutcome":"","variants":[{"id":"old"},{"id":"new"}]},{"id":"RelatedVariants","start":"2016-01-15","expiry":"2016-02-15","author":"Maria Chiorean","description":"Gets related content in using 3 new variants","audience":0.08,"audienceOffset":0.2,"successMeasure":"","audienceCriteria":"All users","dataLinkNames":"","idealOutcome":"","variants":[{"id":"control"},{"id":"tags-only"},{"id":"tags-headline"},{"id":"in-body-links"}]},{"id":"IdentityRegisterV2","start":"2016-01-27","expiry":"2016-03-01","author":"James Pamplin","description":"New user registration page variant for Identity","audience":0.2,"audienceOffset":0.5,"successMeasure":"More people register","audienceCriteria":"everyone","dataLinkNames":"","idealOutcome":"More people register","variants":[{"id":"A"}]},{"id":"IdentitySignInV2","start":"2015-12-15","expiry":"2016-03-01","author":"James Pamplin","description":"New sign in page variants for Identity","audience":0.2,"audienceOffset":0.5,"successMeasure":"More people sign in","audienceCriteria":"everyone","dataLinkNames":"","idealOutcome":"More people sign in","variants":[{"id":"A"},{"id":"B"}]},{"id":"RtrtEmailFormArticlePromoV2","start":"2015-12-17","expiry":"2016-02-03","author":"Gareth Trufitt","description":"Test promotion of email form at bottom vs three paragraphs from end of article pages (when clicked from front)","audience":1,"audienceOffset":0,"successMeasure":"Increase email sign-up numbers","audienceCriteria":"Visitors hitting articles after visiting a front","dataLinkNames":"","idealOutcome":"Email sign-up is increased","variants":[{"id":"bottom-of-page"},{"id":"three-paras-from-bottom"}]},{"id":"PrebidPerformance","start":"2016-01-15","expiry":"2016-01-31","author":"Jimmy Breck-McKye","description":"run prebid.js header-bidding auctions before displaying DFP advertising","audience":0.02,"audienceOffset":0.1,"successMeasure":"","audienceCriteria":"All users","dataLinkNames":"","idealOutcome":"","variants":[{"id":"control"},{"id":"variant"}]},{"id":"LiveblogToast","start":"2015-1-21","expiry":"2016-3-1","author":"Josh Holder","description":"0% AB test that enables liveblog toast notifications","audience":0,"audienceOffset":0,"successMeasure":"","audienceCriteria":"All users","dataLinkNames":"","idealOutcome":"","variants":[{"id":"control"},{"id":"toast"}]},{"id":"UserzoomSurveyMessageV3","start":"2016-01-20","expiry":"2016-02-11","author":"Gareth Trufitt","description":"Segment the userzoom data-team survey","audience":0.2,"audienceOffset":0.7,"successMeasure":"Gain qualitative feedback via a survey","audienceCriteria":"10% of UK visitors to article page, on desktop, that haven't seen the message previously","dataLinkNames":"","idealOutcome":"","variants":[{"id":"control"},{"id":"survey-shown"}]}]};
+    /* eslint-enable */
+    // const getData = () => Promise.resolve(mockData);
+    const getData = () => sendMessageToPage({ action: 'getData' });
+
+    // TEST:
+    // return { getData: () => new Promise((resolve, reject) => setTimeout(() => {
+    //     getData().then(resolve, reject)
+    // }, 40000)) };
+    return { getData };
+};
+
+// TODO:
+// observeOn(Rx.Scheduler.requestAnimationFrame).
+
+const headers = ['id', 'variants'];
+
+const Row = (sources) => {
+    // TODO: Move up one scope
+    const intent = DOM => ({
+        changeVariant$: DOM.select('input').events('change')
+            .map(event => event.target.value)
+    });
+
+    const model = (actions, props$) => {
+        const test$ = props$.map(props => props.test);
+        const initialParticipations$ = props$.map(props => props.initialParticipations);
+        const initialSelectedVariant$ = Rx.Observable.combineLatest(
+            test$, initialParticipations$,
+            (test, initialParticipations) => initialParticipations.get(test.get('id'))
+        );
+        const selectedVariant$ = initialSelectedVariant$.concat(actions.changeVariant$);
+
+        return Rx.Observable.combineLatest(
+            test$, selectedVariant$,
+            (test, selectedVariant) => Im.fromJS({ test, selectedVariant })
+        );
+    };
+
+    const view = state$ => (
+        state$.map(state => {
+            const test = state.get('test');
+            const selectedVariant = state.get('selectedVariant');
+
+            return h('tr', headers.map((header) => (
+                ih('td.mdl-data-table__cell--non-numeric', (
                     header !== 'variants'
                         ? test.get(header)
                         : test.get('variants').map(variant => {
-                            const selectedVariant = participations.get(test.get('id'));
-                            return variantButtonElement(test, variant, selectedVariant);
-                        }));
+                            return h('label.mdl-radio.mdl-js-radio.mdl-js-ripple-effect', [
+                                h('input.mdl-radio__button', {
+                                    type: 'radio',
+                                    name: `${test.get('id')}[variant]`,
+                                    value: variant,
+                                    checked: variant === selectedVariant
+                                }, variant),
+                                variant
+                            ]);
+                        })
+                ))
+            )));
+        })
+    );
 
-            return h('tr', headers.map(cellElement));
-        }
+    const index = sources => {
+        const state$ = model(intent(sources.DOM), sources.props$);
 
-        function view$(tests, participations$) {
-            const rows$ = participations$.map(participations => tests.map(test => rowElement(test, participations)));
+        const value$ = state$.map(state => Im.fromJS({
+            id: state.get('test').get('id'),
+            variant: state.get('selectedVariant')
+        }));
 
-            return rows$.map(rows => {
-                return h('body', [
-                    h('table', {
-                        className: 'mdl-data-table mdl-js-data-table mdl-shadow--2dp'
-                    }, [
-                        h('thead', h('tr', headers.map(key => h('th', { className: 'mdl-data-table__cell--non-numeric' }, key)))),
-                        ih('tbody', rows)
-                    ])
-                ]);
-            });
-        }
-
-        return {
-            view$,
-            intents: {
-                select$
-            }
+        const sinks = {
+            DOM: view(state$),
+            value: value$
         };
-    }
 
+        return sinks;
+    };
 
-    function view() {
-        const table = tableComponent();
-
-        const participations$ = table.intents.select$
-            .startWith({})
-            .scan(
-                (participations, selectedTest) => participations.set(selectedTest.id, selectedTest.variant),
-                initialParticipations
-            );
-
-        // Side effect
-        participations$.subscribe(setParticipations);
-
-        const tree$ = table.view$(initialTests, participations$);
-
-        return { tree$ };
-    }
-
-
-    const v = view();
-
-
-    const out = document.body;
-    const initialDom = virtualize(out);
-
-    v.tree$.
-        startWith(initialDom).
-        bufferWithCount(2, 1).
-        map(([last, current]) => diff(last, current)).
-        scan((out, patches) => patch(out, patches), out).
-        // Material design
-        do(() => window.componentHandler.upgradeDom()).
-        observeOn(Rx.Scheduler.requestAnimationFrame).
-        subscribeOnError((err) => { throw err; });
-
+    return index(sources);
 };
 
-getData().then(render);
+const main = (sources) => {
+    const intent = DOM => ({});
+
+    const model = (actions, tab) => {
+        const dataPromise = tab.getData();
+        const testsDataPromise = dataPromise.then(data => Im.fromJS(data.tests));
+        const participationsDataPromise = dataPromise.then(data => Im.fromJS(data.participations));
+
+        const initialParticipations$ = Rx.Observable.fromPromise(participationsDataPromise);
+        const tests$ = Rx.Observable.fromPromise(testsDataPromise)
+            .map(tests => (
+                tests.map(test => (
+                    test.updateIn(['variants'], variants => (
+                        variants
+                            .map(variant => variant.get('id'))
+                            .push('notintest')
+                    ))
+                ))
+            ));
+
+        return Rx.Observable.combineLatest(
+            tests$, initialParticipations$,
+            (tests, initialParticipations) => Im.fromJS({ tests, initialParticipations })
+        );
+    };
+
+    const view = (state$, rowVTrees$) => (
+        state$.map(() => (
+            // TODO: Centre loading text
+            h('table.mdl-data-table.mdl-js-data-table.mdl-shadow--2dp', [
+                h('thead', h('tr', headers.map(key => h('th.mdl-data-table__cell--non-numeric', key)))),
+                rowVTrees$
+                    .startWith(h('tr', h('td', { attributes: { colspan: 2 } }, 'Loading')))
+                    .map(children => h('tbody', children))
+            ])
+        ))
+    );
+
+    const index = sources => {
+        const state$ = model(intent(sources.DOM), sources.tab);
+
+        const rows$ = state$
+            .map(state => {
+                return state.get('tests').map(test => {
+                    const rowProps$ = Rx.Observable.of({
+                        test,
+                        initialParticipations: state.get('initialParticipations')
+                    });
+                    const row = isolate(Row)({ DOM: sources.DOM, props$: rowProps$ });
+                    return row;
+                }).toJS();
+            })
+            .share();
+
+        const rowVTrees$ = rows$.flatMap(rows => Rx.Observable.combineLatest(...rows.map(row => row.DOM)));
+        const rowValues$ = rows$.flatMap(rows => Rx.Observable.combineLatest(...rows.map(row => row.value)));
+
+        const participations$ = rowValues$
+            .map(participations => (
+                Im.fromJS(participations).reduce((acc, value, key) => {
+                    return acc.set(value.get('id'), value.get('variant'));
+                }, Im.Map())
+            ));
+        const tabData$ = participations$.map(participations => ({ participations }));
+
+        const sinks = {
+            DOM: view(state$, rowVTrees$),
+            tab: tabData$
+        };
+
+        return sinks;
+    };
+
+    return index(sources);
+};
+
+const domDriver = makeDOMDriver('body');
+const drivers = {
+    // We wrap the DOM driver to apply Material Design side effects
+    DOM: vtree$ => {
+        const DOM = domDriver(vtree$.share());
+        // TODO: Why does this run three times?
+        DOM.observable.subscribe(() => window.componentHandler.upgradeDom());
+        return DOM;
+    },
+    tab: tabDriver
+};
+
+Cycle.run(main, drivers);
